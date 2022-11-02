9111c072d365bb45a43925bbdb860e3fedb3071b
Ramazan Doğan <doganrmzn40@gmail.com>
2022-11-02 14:57:33 +0300

    App for react manage statement

---
+++ state-management-app/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <input type="text" placeholder="Güzergah" />
+      <button>Yeni Durak Ekle</button>
+      <hr />
+      <div>
+        <input type="text" placeholder="Durak Adı" />
+        <input type="text" placeholder="Enlem" />
+        <input type="text" placeholder="Boylam" />
+      </div>
+      <hr />
+      <button>Kaydet</button>
+      <br />
+    </>
+  );
+}
+
+export default App;
